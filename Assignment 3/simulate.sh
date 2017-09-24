96a947352152b9ad86d94512520f99ab168544ac
Rahul Kejriwal <kejriwalrahul@outlook.com>
2017-09-24 21:24:38 +0530

    test case corrections

--- Assignment 3/simulate.sh
+++ Assignment 3/simulate.sh
@@ -2,15 +2,15 @@
 	
 	# Change to binary directory
 	cd bin
+	
 	# Convert assembly to binary IR
 	cp ../$1 input
 	java PreProcess > binary_prog
 	rm input
-	cat binary_prog
 	
 	# Load binary IR and simulate execution
-	# ./ProcessorSimulator.o binary_prog ../data_dump
-	# Remove temporary file
-	# rm -f binary_prog
+	./ProcessorSimulator.o binary_prog ../data_dump
+	rm -f binary_prog	
+	
 	# Output the memory dump
-	# cat ../data_dump
+	cat ../data_dump
